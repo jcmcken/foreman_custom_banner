7dddf68b95cca3fb80976fb2ec3beb62204fec90
Jon McKenzie <jcmcken@gmail.com>
2014-10-11 11:11:26 -0400

    Flesh out rest of banner settings

--- app/decorators/controllers/application_controller_decorator.rb
+++ app/decorators/controllers/application_controller_decorator.rb
@@ -1,7 +1,15 @@
 ApplicationController.class_eval do
-  helper_method :custom_banner_text
+  helper_method :custom_banner_text, :custom_banner_enabled, :custom_banner_style
 
   def custom_banner_text
     Setting.custom_banner_text
+  end
+
+  def custom_banner_enabled
+    !!Setting.custom_banner_enabled
+  end
+
+  def custom_banner_style
+    Setting.custom_banner_style
   end
 end
--- app/models/settings/custom_banner.rb
+++ app/models/settings/custom_banner.rb
@@ -5,8 +5,12 @@ class Setting::CustomBanner < ::Setting
 
     Setting.transaction do
       [
-        self.set('custom_banner_text', N_('Text to set on the custom banner'), nil)
-      ].compact.each { |s| self.create s.update(:category => "Setting::CustomBanner") }
+        self.set('custom_banner_text', N_('Text to set on the custom banner'), 
+          'Configure the custom banner in your Foreman settings!'),
+        self.set('custom_banner_enabled', N_('Whether to enable the custom banner'), true),
+        self.set('custom_banner_style', N_('CSS styling for the custom banner'),
+          'text-align:center;background-color:green;color:white;font-weight:bold;'),
+      ].compact.each { |s| self.create! s.update(:category => "Setting::CustomBanner") }
     end
 
     true
