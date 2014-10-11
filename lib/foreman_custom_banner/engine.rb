96238c7de1413b62f469536f288525ae2646f462
Jon McKenzie <jcmcken@gmail.com>
2014-10-11 11:11:40 -0400

    Clean up engine config

--- lib/foreman_custom_banner/engine.rb
+++ lib/foreman_custom_banner/engine.rb
@@ -1,5 +1,6 @@
 module ForemanCustomBanner
   class Engine < ::Rails::Engine
+    isolate_namespace ForemanCustomBanner
 
     config.to_prepare do
       Dir.glob(Rails.root + 'app/decorators/**/*_decorator*.rb').each do |c|
@@ -12,8 +13,10 @@ module ForemanCustomBanner
         (Setting.table_exists? rescue(false))
     end
 
-    initializer 'foreman_custom_banner.load_data' do |app|
-      app.config.paths['db/migrate'] += ForemanCustomBanner::Engine.paths['db/migrate'].existent
+    initializer 'foreman_custom_banner.register_plugin', :after => :finisher_hook do |app|
+      Foreman::Plugin.register :foreman_custom_banner do
+        requires_foreman '>= 1.5'
+      end
     end
   end
 end
